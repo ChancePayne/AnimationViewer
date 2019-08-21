8de3c2c5951370de458056a664de08997b21be40
Chance Payne <chance@lambdaschool.com>
2019-08-21 09:34:48 -0700

    Initial commit

---
+++ app/src/main/java/com/lambdaschool/animationviewer/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.lambdaschool.animationviewer
+
+import android.annotation.SuppressLint
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongThread")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
