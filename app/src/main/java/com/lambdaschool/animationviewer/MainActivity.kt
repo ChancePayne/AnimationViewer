ed4dfcf1c4ed6d7dc7d25ff15c1c82701e8991bf
Chance Payne <chance@lambdaschool.com>
2019-08-21 10:13:39 -0700

    create and work with an animation drawable

--- app/src/main/java/com/lambdaschool/animationviewer/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/animationviewer/MainActivity.kt
@@ -3,9 +3,11 @@ package com.lambdaschool.animationviewer
 import android.annotation.SuppressLint
 import android.graphics.ImageDecoder
 import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.AnimationDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,10 +18,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // add and animate a gif
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             val gifDrawable = ImageDecoder.decodeDrawable(ImageDecoder.createSource(resources, R.drawable.walking_duck))
             animated_image.setImageDrawable(gifDrawable)
             (gifDrawable as AnimatedImageDrawable).start()
-        }
+        }*/
+
+        // use an animated drawable
+        val frameDrawable = ContextCompat.getDrawable(this, R.drawable.toaster_animated)
+        animated_image.setImageDrawable(frameDrawable)
+        (frameDrawable as AnimationDrawable).start()
     }
 }
